a5d8e0bcb58b721a41c5bff612fae343c652c3b7
Maximilian Bachmeier <git.bachmeierm@outlook.com>
2018-01-20 19:05:39 +0100

    Add ParkourMovementComponent

---
+++ Plugins/ParkourPlugin/Source/ParkourPlugin/Private/ParkourMovementComponent.cpp
@@ -0,0 +1,1 @@
+#include "ParkourMovementComponent.h"
---
+++ Plugins/ParkourPlugin/Source/ParkourPlugin/Public/ParkourMovementComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "ParkourMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARKOURPLUGIN_API UParkourMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
